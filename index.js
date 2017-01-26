4e47b236231cbf86cc1bde978dee6245c76a5ba9
Huy Dang <huydn@mobilus.co.jp>
2017-01-26 18:11:18 +0900

    Using newest version of dependencies

--- index.js
+++ index.js
@@ -10,114 +10,113 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function (sails) {
 
-  return {
+    return {
 
-    defaults: {
-      __configKey__: {
-        config: {
-          entry: [path.resolve(__dirname, '../../assets/src/main.js')],
-          output: {
-            path: path.resolve(__dirname, '../../.tmp/public/'),
-            publicPath: '/',
-            filename: 'js/bundle.js'
-          },
-          resolveLoader: {
-            root: path.join(__dirname, '../../node_modules')
-          },
-          plugins: [
-            new webpack.optimize.OccurenceOrderPlugin(),
-            new webpack.NoErrorsPlugin(),
-            new CleanWebpackPlugin(['public'], {
-              root: path.join(__dirname, '../../.tmp'),
-              verbose: false
-            }),
-            new CopyWebpackPlugin([{
-              from: path.resolve(__dirname, '../../src/assets/'),
-              to: 'assets'
-            }]),
-            new CleanWebpackPlugin(['src'], {
-                root: path.join(__dirname, '../../.tmp/public'),
-                verbose: false
-            })
-          ],
-          module: {
-            loaders: [
-              {
-                test: /\.vue$/,
-                loader: 'vue'
-              },
-              {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/
-              },
-              {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file',
-                query: {
-                  name: '[name].[ext]?[hash]'
+        defaults: {
+            __configKey__: {
+                config: {
+                    entry: [path.resolve(__dirname, '../../assets/src/main.js')],
+                    output: {
+                        path: path.resolve(__dirname, '../../.tmp/public/'),
+                        publicPath: '/',
+                        filename: 'js/bundle.js'
+                    },
+                    resolveLoader: {
+                        root: path.join(__dirname, '../../node_modules')
+                    },
+                    plugins: [
+                        new webpack.optimize.OccurenceOrderPlugin(),
+                        new webpack.NoErrorsPlugin(),
+                        new CleanWebpackPlugin(['public'], {
+                            root: path.join(__dirname, '../../.tmp'),
+                            verbose: process.env.NODE_ENV === 'development'
+                        }),
+                        new CopyWebpackPlugin(
+                            [
+                                { from: 'assets/favicon.ico'},
+                                { from: 'assets/robots.txt'},
+                                { from: 'assets/images', to: 'images'},
+                                { from: 'assets/js', to: 'js'},
+                                { from: 'assets/styles', to: 'styles'}
+                            ]
+                        ),
+                    ],
+                    module: {
+                        loaders: [
+                            {
+                                test: /\.vue$/,
+                                loader: 'vue'
+                            },
+                            {
+                                test: /\.js$/,
+                                loader: 'babel',
+                                exclude: /node_modules/
+                            },
+                            {
+                                test: /\.(png|jpg|gif|svg)$/,
+                                loader: 'file',
+                                query: {
+                                    name: '[name].[ext]?[hash]'
+                                }
+                            }
+                        ]
+                    },
+                    devServer: {
+                        historyApiFallback: true,
+                        noInfo: true
+                    },
+                    devtool: '#eval-source-map'
+                },
+                devServerConfig: {
+                    filename: "js/bundle.js",
+                    proxy: {
+                        "*": "http://localhost:1337"
+                    },
+                    watchOptions: {
+                        hot: true,
+                        inline: true,
+                        port: 3000
+                    },
+                    quiet: true
                 }
-              }
-            ]
-          },
-          devServer: {
-            historyApiFallback: true,
-            noInfo: true
-          },
-          devtool: '#eval-source-map'
+            }
         },
-        devServerConfig: {
-          filename: "js/bundle.js",
-          proxy: {
-            "*": "http://localhost:1337"
-          },
-          watchOptions: {
-            hot: true,
-            inline: true,
-            port: 3000
-          },
-          quiet: true
-        }
-      }
-    },
 
-    initialize: function (cb) {
-      var config = this.defaults.vuewebpack.config;
-      var devServerConfig = this.defaults.vuewebpack.devServerConfig;
+        initialize: function (cb) {
+            var config = this.defaults.vuewebpack.config;
+            var devServerConfig = this.defaults.vuewebpack.devServerConfig;
 
-      // Configure webpack dev server
-      if (process.env.NODE_ENV === 'development') {
-        config.output.publicPath = 'http://localhost:3000/';
-        config.entry.unshift("webpack-dev-server/client?http://0.0.0.0:3000/", "webpack/hot/only-dev-server");
-        config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
-      }
-      else {
-        config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}));
-      }
+            // Configure webpack dev server
+            if (process.env.NODE_ENV === 'development') {
+                config.output.publicPath = 'http://localhost:3000/';
+                config.entry.unshift("webpack-dev-server/client?http://0.0.0.0:3000/", "webpack/hot/only-dev-server");
+                config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+            }
+            else {
+                config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}));
+            }
 
-      // Create Webpack compiler
-      var compiler = webpack(config, function(err, stats) {
-        if (err) throw err;
-        if (process.env.NODE_ENV === 'development') {
-          // Watch for changes
-          compiler.watch({ aggregateTimeout: 300 }, function(err, stats) {
-            if (err) throw err;
-            // Create Webpack dev server
-            devServer = new webpackDevServer(compiler, devServerConfig);
-            devServer.listen(3000);
-            return cb();
-          });
+            // Create Webpack compiler
+            var compiler = webpack(config, function(err, stats) {
+                if (err) throw err;
+                if (process.env.NODE_ENV === 'development') {
+                    // Watch for changes
+                    compiler.watch({ aggregateTimeout: 300 }, function(err, stats) {
+                        if (err) throw err;
+                        // Create Webpack dev server
+                        devServer = new webpackDevServer(compiler, devServerConfig);
+                        devServer.listen(3000);
+                        return cb();
+                    });
+                }
+                else {
+                    // Run production build
+                    compiler.run(function (err, stats) {
+                        if (err) throw err;
+                        return cb();
+                    });
+                }
+            });
         }
-        else {
-          // Run production build
-          compiler.run(function (err, stats) {
-            if (err) throw err;
-            return cb();
-          });
-        }
-      });
     }
-
-  }
-
 };
